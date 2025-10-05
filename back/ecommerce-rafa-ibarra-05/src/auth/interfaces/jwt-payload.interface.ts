9d9dd64a40376deb5c0b650a5309de1b62839eba
rafael ibarra <rafaelibarra0795@gmail.com>
2025-10-05 20:39:55 -0300

    Limpieza de codigo

--- back/ecommerce-rafa-ibarra-05/src/auth/interfaces/jwt-payload.interface.ts
+++ back/ecommerce-rafa-ibarra-05/src/auth/interfaces/jwt-payload.interface.ts
@@ -1,9 +1,9 @@
 import { Role } from '../roles.enum';
 
 export interface JwtPayload {
-  sub: string; // ID del usuario
+  sub: string;
   email: string;
-  role: Role; // 👈 único rol, tipado con enum
-  iat?: number; // opcional: issued at
-  exp?: number; // opcional: expiration
+  role: Role;
+  iat?: number;
+  exp?: number;
 }
